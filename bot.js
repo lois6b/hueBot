91c64af1d12421ebd9da36251b4afa4e92eeb29d
Luis Martin <lmartinmarti@dxc.com>
2017-10-04 16:01:47 +0200

    no 'response'

--- bot.js
+++ bot.js
@@ -25,5 +25,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  console.log('Received your message: '+ msg);
 });
