89079f8e6461999373aa18463af27a727c7f5d97
Luis Martin <lmartinmarti@dxc.com>
2017-10-04 17:16:19 +0200

    muchos cambios

--- bot.js
+++ bot.js
@@ -1,5 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+var mensajes = ["Comeme los huevos", 
+				"Me saben los huevos a aceituna", 
+				"Programo en Java y te meto la raba", 
+				"Me comes el huevo en enero si no es primero",
+				"Vale, me has pillado, soy un bot... cómeme los bytes",
+				"Si reordenas las letras de 'TOM MARBOLO RIDDLE' se convierte en 'CÓMEME LOS HUEVOS'",
+				"Soy un tipo educado: cuando llego a un sitio siempre doy los huevos días",
+				"Fui con otros cinco amigos al desierto e hicimos una sombra de tres pares de huevos",
+				"Las gallinas quieren mucho a sus hijos: les cuesta un huevo tenerlos",
+				"Chúpame los huevos y déjamelos como nuevos",
+				"Qué bien me comió los huevos el puente",
+				"Serás un ser más longevo si primero me comes el huevo",
+				"De puente a puente y me comes los huevos de frente"
+				];
+				
+var comandos= ["/start",
+			   "/echo"
+			  ];
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '428109668:AAGHXQ2kHCXS2dAA1wiyJabNozxz9Czw8TQ';
 
@@ -14,16 +31,56 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-
+	//console.log(msg);
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
+bot.onText(/\/start/, (msg) => {
+
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Bienvenido a mis huevos redondos");
+});
+
+
+/*bot.on('sticker', (msg) => {
+	const chatId = msg.chat.id;
+    bot.sendPhoto(chatId, "http://i.imgur.com/VRYdhuD.png");
+});*/
+
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  console.log('Received your message: '+ msg);
+	if( !msg.text.startsWith("/")){
+		console.log('Received your message: ' + msg.text + " - From: " + msg.from.first_name);
+		const chatId = msg.chat.id;
+		if(msg.text.includes("vives")){
+			bot.sendLocation(chatId, 43.2351181, -5.7755287);
+		}else{
+			bot.sendMessage(chatId, generateText());
+		}
+	}
 });
+
+
+
+
+function generateText(){
+	
+	//10% of the time it will say: 
+	var txt10 ="En el cielo las estrellas, en el suelo el alambrado. Y en la raja de tu culo mi chorizo colorado";
+
+	if(Math.random() > 0.8){
+		//.log(txt)
+		return txt10;
+	}else{
+		return rndMensaje(mensajes);
+	}
+	
+}
+
+function rndMensaje(array){
+	
+	return array[Math.floor(Math.random()*array.length)];
+}
